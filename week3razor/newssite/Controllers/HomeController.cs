da7c401c00d1899824eb5031d3e7ec5d2996917a
Finnvdh01 <39246@ma-web.nl>
2025-05-27 12:37:45 +0200

    commit 14

--- week3razor/newssite/Controllers/HomeController.cs
+++ week3razor/newssite/Controllers/HomeController.cs
@@ -7,22 +7,43 @@ namespace newssite.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private List<NewsItem> newsItems = new List<NewsItem>();
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
-        NewsItem item = new NewsItem()
+        newsItems.Add(new NewsItem()
         {
             Title = "mijn eerste model",
             Content = "was niet zo moeilijk om te maken.",
             imageUrl = "img/newsitem.png"
-        };
+        });
 
-        return View(item);
+        newsItems.Add(new NewsItem()
+        {
+            Title = "mijn tweede model",
+            Content = "was ook niet zo moeilijk om te maken.",
+            imageUrl = "img/newsitem.png"
+        });
+
+        newsItems.Add(new NewsItem()
+        {
+            Title = "mijn derde model",
+            Content = "was ook niet zo moeilijk om te maken.",
+            imageUrl = "img/newsitem.png"
+        });
+
+        newsItems.Add(new NewsItem()
+        {
+            Title = "mijn vierde model",
+            Content = "was ook niet zo moeilijk om te maken.",
+            imageUrl = "img/newsitem.png"
+        });
+    }
+
+    public IActionResult Index()
+    {
+        return View(newsItems);
     }
 
     public IActionResult Privacy()
